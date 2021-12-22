98e064bf9ebf6e0e373cea87e1a7df19ee3fe603
Aut0Matic <82569441+Aut0Matic@users.noreply.github.com>
2021-12-22 19:33:04 +1030

    Day 1 complete

--- Day 1 Sonar Sweep/solution.py
+++ Day 1 Sonar Sweep/solution.py
@@ -1,6 +1,26 @@
 import os
-#text_file = open("input.txt", 'r')
 
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-for f in files:
-    print(f)
+list = []
+
+with open('input.txt') as file:
+    for line in file:
+        list.append(int(line.rstrip()))
+        
+count = 0
+
+for i in range(1,len(list)):
+    if(list[i]>list[i-1]):
+        count=count + 1
+
+windowcount = 0
+i = 3
+while i < len(list):
+    prevwindow = list[i-3] + list[i-2] + list[i-1]
+    newwindow = list[i-2] + list[i-1] + list[i]
+    
+    if newwindow>prevwindow:
+        windowcount = windowcount + 1
+    i = i + 1
+
+print("Part 1: ", count)
+print("Part 2: ", windowcount)
