680912d9757fc3b89c3e9686d262f3e53e66ec52
Austen Lindsay <82569441+Aut0Matic@users.noreply.github.com>
2023-12-04 21:05:16 +1030

    Day 4

---
+++ 2023/Day 4/Day_4.py
@@ -0,0 +1,44 @@
+import re
+
+# Results variables
+r = r2 = 0
+
+# Parsing the input into a matrix
+matrix = [x for x in open('Day_4.txt').read().strip().split('\n')]
+
+# Vector to track how many of each card we have
+cardcounts = [1] * len(matrix)
+
+for index, line in enumerate(matrix, start = 0):
+    a, b = line.split('|')
+    a = re.findall('\d+',a)
+    a = a[1:] # winning numbers
+    b = re.findall('\d+', b) # card numbers
+    
+    
+    score = matches = 0
+    
+    # Search through the numbers in this card
+    for num in b:
+        
+        # If the numbers is a match
+        if num in a:
+            
+            # Keep track of it
+            matches += 1
+            
+            # Correctly adjust the score
+            if score < 1:
+                score = 1
+            else:
+                score = score * 2
+    r+= score
+
+    # Update the number of each card
+    for i in range(1,matches+1):
+        cardcounts[index+i] += cardcounts[index]
+
+r2 = sum(cardcounts)
+
+print("Part 1:", r)
+print("Part 2:", r2)
